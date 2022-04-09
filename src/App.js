6edaf6b1fa39f6de200a233ba25963622b0b10d0
idoodi <idoodi@ya.ru>
2022-04-09 20:48:11 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({})
+    }, 100)
+  })
+
   return (
     <div>
-
+      {data && <div style={{color: 'red'}}>data</div>}
+      <h1>Hello world</h1>
+      <button></button>
+      <input placeholder="input value"></input>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('TEST APP', () => {
+
+  test('sync render test', () => {
+    render(<App />);
+
+    // getBy возвращает один элемент/ошибку при отсутствии
+    // getAll возвращает массив элементов
+    // query(By, All) - возвращает элементы/null при отсутствии
+
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole(/button/i);
+    const input = screen.getByPlaceholderText(/input value/i);
+
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+
+    //показывает в консоли что рендерится
+    // screen.debug()
+
+    // expect(input).toMatchSnapshot();
+
+  });
+
+  test('async render test', async () => {
+    render(<App />);
+
+    // find(By, All) - возвращает элементы/ничего при отсутствии | обьект завернут в промис
+    const helloWorldElem = await screen.findByText(/data/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(helloWorldElem).toHaveStyle({ color: 'red' });
+
+  });
+
+
+})
